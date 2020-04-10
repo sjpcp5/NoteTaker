b41ca88735d96e41a7636d240f353c83f3c19a6f
saphirah.pociluyko <saphirah.j.pociluyko@usace.army.mil>
2020-04-10 17:50:08 -0500

    added routes and server.js

--- server.js
+++ server.js
@@ -1,124 +1,39 @@
-const express = require("express");
-const mongojs = require("mongojs");
-const logger = require("morgan");
-const path = require("path");
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
 
-const app = express();
+var express = require("express");
 
-app.use(logger("dev"));
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
 
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
-const databaseUrl = process.env.MONGODB_URI || "notetaker";
-const collections = ["notes"];
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-const db = mongojs(databaseUrl, collections);
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
 
-db.on("error", error => {
-  console.log("Database Error:", error);
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "./public/index.html"));
-});
-
-app.post("/submit", (req, res) => {
-  console.log(req.body);
-
-  db.notes.insert(req.body, (error, saved) => {
-    if (error) {
-      console.log(error);
-    } else {
-      res.send(saved);
-    }
-  });
-});
-
-app.get("/all", (req, res) => {
-  db.notes.find({}, (error, found) => {
-    if (error) {
-      console.log(error);
-    } else {
-      res.json(found);
-    }
-  });
-});
-
-app.get("/find/:id", (req, res) => {
-  db.notes.findOne(
-    {
-      _id: mongojs.ObjectId(req.params.id)
-    },
-    (error, found) => {
-      if (error) {
-        console.log(error);
-        res.send(error);
-      } else {
-        console.log(found);
-        res.send(found);
-      }
-    }
-  );
-});
-
-app.post("/update/:id", (req, res) => {
-  db.notes.update(
-    {
-      _id: mongojs.ObjectId(req.params.id)
-    },
-    {
-      $set: {
-        title: req.body.title,
-        note: req.body.note,
-        modified: Date.now()
-      }
-    },
-    (error, edited) => {
-      if (error) {
-        console.log(error);
-        res.send(error);
-      } else {
-        console.log(edited);
-        res.send(edited);
-      }
-    }
-  );
-});
-
-app.delete("/delete/:id", (req, res) => {
-  db.notes.remove(
-    {
-      _id: mongojs.ObjectID(req.params.id)
-    },
-    (error, removed) => {
-      if (error) {
-        console.log(error);
-        res.send(error);
-      } else {
-        console.log(removed);
-        res.send(removed);
-      }
-    }
-  );
-});
-
-app.delete("/clearall", (req, res) => {
-  db.notes.remove({}, (error, response) => {
-    if (error) {
-      console.log(error);
-      res.send(error);
-    } else {
-      console.log(response);
-      res.send(response);
-    }
-  });
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Application running on PORT ${PORT}`);
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });
